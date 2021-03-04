3cc57039fd20df4530895b7c3a2bf22bac008ef8
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2021-03-04 18:18:47 -0300

    adding exercise 3.

---
+++ parte-2/exercicio-3/app/src/main/java/com/example/exercicio_3/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.exercicio_3;
+
+import android.Manifest;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button getGPSBtn = findViewById(R.id.getGPSBtn);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+        getGPSBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GPSTracker g = new GPSTracker(getApplicationContext());
+                Location l = g.getLocation();
+                if (l != null) {
+                    double lat = l.getLatitude();
+                    double longi = l.getLongitude();
+                    Toast.makeText(getApplicationContext(), "LAT: " + lat + " LONG: " +
+                            longi, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
